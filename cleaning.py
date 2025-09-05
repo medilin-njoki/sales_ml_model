5d6fc23fd93e281b357ffeae7311067be69b592c
medilin <medilinnjoki@icloud.com>
2025-09-05 11:02:23 +0200

    update cleaning

--- cleaning.py
+++ cleaning.py
@@ -3,4 +3,6 @@ def remove_closed_stores(df):
     return df[df['Open'] == 1]
 
 def fix_holidays_column(df):
-    return df['State_holiday'].replace({'a': 1, 'b': 1, 'c': 1, '0': 0}).astype(int)
+    df['State_holiday'] = df['State_holiday'].replace({'a': 1, 'b': 1, 'c': 1, '0': 0})
+    df['State_holiday'] = df['State_holiday'].astype(int)
+    return df
